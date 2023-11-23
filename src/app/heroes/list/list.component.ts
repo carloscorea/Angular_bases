08dc6a099e39465caf1b46a1cecb0239148ea738
Carlos Corea <car.corea@gmail.com>
2023-11-23 15:15:18 -0600

    Fin de sección 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  public heroNames:string[]=['Spiderman','Iroman', 'Hulk', 'She hulk', 'Thor']
+  public deletehero?:string;
+
+  removeLastHero():void{
+    this.deletehero = this.heroNames.pop();
+
+  }
+}
